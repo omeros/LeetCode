c1208e1bcad59c4d0f8a20b437a914a8255da64b
omeros <omer.golan.dev@gmail.com>
2026-01-11 01:44:52 +0200

    `Added isSameTree function and notSameKids helper function to compare two binary trees`

--- probllem-list.ts
+++ probllem-list.ts
@@ -874,4 +874,54 @@ function innerInorderTraversal(root: TreeNode | null,arr: number[]): number[] {
    
 };
 //*******************************************************************************************/
-//**************************** **********************************************/
+//**************************** 100. Same Tree **********************************************/
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    if (p.val!==q.val) {
+        console.log('p.val!==q.vall')
+        return false
+    }
+    if(((!p.left)&&(!p.right))&&((!q.left)&&(!q.right))) return true;
+    if(p.val!==q.val){
+        console.log('p.val!==q.val')
+        return false;
+    }
+    if(!notSameKids(p,q)){
+        console.log('notSameKids3')
+        return false;
+    }
+    let result = null ;
+    if(p.left&&p.right)  {
+        return  isSameTree(p.left,q.left)&&isSameTree(p.right,q.right)
+    }else if(p.left&&!p.right){
+            return isSameTree(p.left,q.left)
+    }else if(!p.left&&p.right){
+            return isSameTree(p.right,q.right)
+    }
+}
+
+function notSameKids(p,q){
+    if( ((p.left)&&(p.right)) && ((!q.left)||(!q.right))  ){
+        console.log('notSameKids1')
+        return false
+    }else  if( ((!p.left)&&(q.left)) || ((p.left)&&(!q.left)) || ((p.right)&&(!q.right)) || ((p.right)&&(!q.right)) ){
+        console.log('notSameKids2')
+        return false
+    }
+    return true
+}
+
+//*******************************************************************************************/
